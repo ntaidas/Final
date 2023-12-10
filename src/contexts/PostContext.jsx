5ffe699c1e46c01af4fdc3682ca0c50b9ca89336
AidasD <ntaidas@gmail.com>
2023-12-11 01:45:00 +0200

    post context

---
+++ src/contexts/PostContext.jsx
@@ -0,0 +1,71 @@
+import { createContext, useEffect, useReducer } from "react";
+
+const PostsContext = createContext();
+const PostActionTypes = {
+  getPosts: "get all posts",
+  newPost: "Write new post",
+  deletePost: "delete the post",
+  edit: "edit post",
+};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case PostActionTypes.getPosts:
+      return action.data;
+    case PostActionTypes.newPost:
+      fetch(`http://localhost:8888/posts`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(action.data),
+      });
+      return [...state, action.data];
+    case PostActionTypes.deletePost:
+      fetch(`http://localhost:8888/posts/${action.id}`, {
+        method: "DELETE",
+      });
+      return state.filter((el) => el.id.toString() !== action.id.toString());
+    case PostActionTypes.edit:
+      fetch(`http://localhost:8888/posts/${action.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(action.data),
+      });
+      return state.map((el) => {
+        if (el.id.toString() === action.id.toString()) {
+          return { id: action.id, ...action.data };
+        } else {
+          return el;
+        }
+      });
+    default:
+      console.log("error: no such action", action.type);
+      return state;
+  }
+};
+
+const PostsProvider = ({ children }) => {
+  const [posts, setPosts] = useReducer(reducer, []);
+
+  useEffect(() => {
+    fetch(`http://localhost:8080/videoGames`)
+      .then((res) => res.json())
+      .then((data) =>
+        setPosts({
+          type: PostActionTypes.getPosts,
+          data: data,
+        })
+      );
+  }, []);
+
+  return (
+    <PostsContext.Provider value={{ posts, setPosts, PostActionTypes }}>
+      {children}
+    </PostsContext.Provider>
+  );
+};
+
+export { PostsProvider};
+export default PostsContext;
