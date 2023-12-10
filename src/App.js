92e31360e9544ec1a8e941b665c7f65ac97cf5d0
AidasD <ntaidas@gmail.com>
2023-12-10 17:00:27 +0200

    added LoginPage component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/ui/header/Header";
 import Footer from "./components/ui/footer/Footer";
+import Home from "./components/pages/home/Home";
 
 function App() {
   return (
     <>
       <Header />
-      
+      <Routes>
+        <Route index element={<Home/>}/>
+      </Routes>
       <Footer />
     </>
   );
