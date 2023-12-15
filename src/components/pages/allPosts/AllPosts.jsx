6ab9f6bada3566303b62540557aa0090bf8aeed2
AidasD <ntaidas@gmail.com>
2023-12-15 11:57:43 +0200

    some styling changes

--- src/components/pages/allPosts/AllPosts.jsx
+++ src/components/pages/allPosts/AllPosts.jsx
@@ -7,9 +7,10 @@ const StyledFeed = styled.main`
 display:flex;
 flex-direction:column;
 align-items: center;
+margin: 50px 0 100px;
 gap: 2rem;
 div{
-  width:300px;
+  width:500px;
 }`;
 
 const AllPosts = () => {
